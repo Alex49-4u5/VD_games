0c344a7910376bb6b5884db416335e3437886668
Alex49-4u5 <mrluzer2@gmail.com>
2026-03-18 22:19:29 +0300

    feat: add ruff linter and even game

--- VD_games/cli.py
+++ VD_games/cli.py
@@ -1,11 +1,14 @@
-"""Приколы для приветсвия."""
+"""Модуль для интeрфейса командной строки игры"""
 import prompt
 
+
 def welcome_user():
-    """Приветсвие"""
+    """Функция приветствия пользователя"""
     print("Welcome to the VD-games!")
     name = prompt.string('May I have your name? ')
     print(f"Hello, {name}!")
+    return name
+
 
 if __name__ == "__main__":
     welcome_user()
---
+++ VD_games/scripts/VD_even.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+"""Проверка на чётность"""
+import random
+from VD_games.cli import welcome_user
+
+
+def is_even(number):
+    """Является ли число чётным"""
+    return number % 2 == 0
+
+
+def play_game():
+    """Основ логика игры"""
+    name = welcome_user()  # Возвращает имя, а не печатает его
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+
+    correct_answers = 0
+    rounds_to_win = 3
+
+    while correct_answers < rounds_to_win:
+        number = random.randint(1, 100)
+        print(f"Question: {number}")
+
+        answer = input("Your answer: ").strip().lower()
+
+        correct_answer = "yes" if is_even(number) else "no"
+
+        if answer == correct_answer:
+            print("Correct!")
+            correct_answers += 1
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {name}!")
+            return
+
+    print(f"Congratulations, {name}!")
+
+
+def main():
+    """Точка входа"""
+    print("Welcome to the VD-games!")
+    play_game()
+
+
+if __name__ == "__main__":
+    main()
